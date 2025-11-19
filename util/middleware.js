b09db8cc6b001ea002b9bdb4bb843da8c8689ce5
Dardan <dardangerguri@outlook.com>
2025-11-19 23:37:26 +0200

    ex13.9-BlogsSQL: Add email validation for usernames and improve error handling

--- util/middleware.js
+++ util/middleware.js
@@ -1,8 +1,10 @@
 const errorHandler = (error, req, res, next) => {
   console.error(error);
 
-  if (error.name === 'SequelizeValidationError')
-    return res.status(400).json({ error: 'Required field is missing' })
+  if (error.name === 'SequelizeValidationError') {
+    const messages = error.errors.map(e => e.message)
+    return res.status(400).json({ error: messages })
+  }
   if (error.name === 'SequelizeDatabaseError')
     return res.status(400).json({ error: 'Database error' })
   if (error.name === 'NotFound')
